cafac0c02bb22644c35dd9ef37be357b29ace404
Rogoberto Ribeiro <usbetin@gmail.com>
2020-10-14 01:35:36 +0000

    13out

--- controllers/acd_credito_cobranca.py
+++ controllers/acd_credito_cobranca.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 def index():
-    projeto = db.projeto(request.args(0, cast=int))
     
-    rows = db((db.boleto_atrasado.projeto == request.args(0, cast=int))&(db.boleto_atrasado.status == 'OVERDUE')).select(orderby=db.boleto_atrasado.due_date)
+    rows = db(db.boleto_atrasado).select()
     #grid = SQLFORM.smartgrid(db.boleto)
     return dict(rows=rows)
